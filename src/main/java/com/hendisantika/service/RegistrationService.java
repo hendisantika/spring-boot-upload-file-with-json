1bda5171931051b23277a32ce71cc643f0fdfda2
hendisantika <hendisantika@yahoo.co.id>
2023-08-11 09:37:20 +0700

    #7 - Add RegistrationService ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/service/RegistrationService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-upload-file-with-json
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/11/23
+ * Time: 09:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class RegistrationService {
+    private final FileService fileService;
+}
