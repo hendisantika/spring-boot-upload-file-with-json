006c13c91076546abe0267746b21272394505584
hendisantika <hendisantika@yahoo.co.id>
2023-08-11 09:38:01 +0700

    #8 - Add registerUser in RegistrationService ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/service/RegistrationService.java
+++ src/main/java/com/hendisantika/service/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.hendisantika.service;
 
+import com.hendisantika.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +18,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RegistrationService {
     private final FileService fileService;
+
+    public void registerUser(UserDTO userDTO) {
+        //TODO Add code to store user basic details like name, address etc
+        // 2L should be replaced by dynamic user id which will be generated after storing user details
+        fileService.saveFile
+                (userDTO.getAddressProof().getFile(),
+                        getFilePath("F:\\TEST",
+                                userDTO.getAddressProof().getDocumentType(), 2L));
+    }
 }
