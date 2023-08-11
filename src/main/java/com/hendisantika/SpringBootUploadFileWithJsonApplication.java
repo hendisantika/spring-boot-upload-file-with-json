c4f793bcb91f8699bf83b91919ca8983549bd5a9
hendisantika <hendisantika@yahoo.co.id>
2023-08-11 10:12:42 +0700

    #13 - Enable ObjectMapper ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/SpringBootUploadFileWithJsonApplication.java
+++ src/main/java/com/hendisantika/SpringBootUploadFileWithJsonApplication.java
@@ -1,7 +1,9 @@
 package com.hendisantika;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootUploadFileWithJsonApplication {
@@ -10,4 +12,8 @@ public class SpringBootUploadFileWithJsonApplication {
         SpringApplication.run(SpringBootUploadFileWithJsonApplication.class, args);
     }
 
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
 }
