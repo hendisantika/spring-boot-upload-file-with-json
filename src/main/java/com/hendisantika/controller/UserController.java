cc688a2b6c130eb22eca23fa6eee2ee99c841d5c
hendisantika <hendisantika@yahoo.co.id>
2023-08-11 10:27:06 +0700

    #18 - Add uploadFile in UserController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/controller/UserController.java
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.hendisantika.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.dto.User;
 import com.hendisantika.dto.UserDTO;
+import com.hendisantika.service.FileService;
 import com.hendisantika.service.RegistrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final RegistrationService registrationService;
+    private final FileService fileService;
 
     @PostMapping("register")
     public ResponseEntity<String> registerUser(@ModelAttribute UserDTO userDTO) {
@@ -33,5 +37,21 @@ public class UserController {
         return new ResponseEntity<>
                 ("User registered successfully.",
                         HttpStatus.CREATED);
+    }
+
+    private final ObjectMapper objectMapper;
+
+    @PostMapping("/json-object-file-upload")
+    public User uploadFile(@RequestParam("model") String jsonObject, @RequestParam("file") MultipartFile file) {
+        User response = null;
+        try {
+            String fileName = fileService.storeFile(file);
+            ServletUriComponentsBuilder.fromCurrentContextPath().path(fileName).toUriString();
+            response = objectMapper.readValue(jsonObject, User.class);
+            response.setImage(file.getOriginalFilename());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return response;
     }
 }
