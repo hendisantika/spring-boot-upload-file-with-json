305b83f4bc222b89f6dd2451e25106da189c214c
hendisantika <hendisantika@yahoo.co.id>
2023-08-11 10:26:32 +0700

    #17 - Add storeFile in FileService ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/service/FileService.java
+++ src/main/java/com/hendisantika/service/FileService.java
@@ -1,12 +1,17 @@
 package com.hendisantika.service;
 
+import com.hendisantika.config.FileStorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +37,35 @@ public class FileService {
             System.out.println("Error while storing file " + e);
         }
     }
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileService(FileStorageProperties fileStorageProperties) {
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("Couldn't create the directory where the upload files will be saved.", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        // Normalize file name
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            // Check if the file's name contains valid  characters or not
+            if (fileName.contains("..")) {
+                throw new RuntimeException("Sorry! File name which contains invalid path sequence " + fileName);
+            }
+            // Copy file to the target place (Replacing existing file with the same name)
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return fileName;
+    }
+
 }
